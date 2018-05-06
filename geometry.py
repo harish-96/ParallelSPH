682d269e823d73c4f2d3c5cd9e55a2e4c585d92e
Harish Murali <harish02murali@gmail.com>
2018-05-06 22:37:09 +0530

    SPH time change

--- geometry.py
+++ geometry.py
@@ -11,14 +11,15 @@ import matplotlib.pyplot as plt
 
 #TEST CASE 3: Airfoil with walls
 x1, y1 = geom.get_4digit_naca_airfoil(dx=0.01)
-x2, y2 = geom.get_2d_wall(dx = 0.01,center=[0.5, -0.01], length=3)
-x3, y3 = geom.get_2d_wall(dx = 0.01,center=[0.5, 1.01], length=3)
+x2, y2 = geom.get_2d_wall(dx = 0.01,center=[0.5, -0.1], length=3)
+x3, y3 = geom.get_2d_wall(dx = 0.01,center=[0.5, 1.1], length=3)
 x = np.concatenate((x3, x2, 0.5 + 0.5 * x1))
 y = np.concatenate((y3, y2, 0.5 + 0.5 * y1))
+
+
 plt.scatter(x, y)
 plt.axis('equal')
 plt.show()
-
 
 with open('airfoil_wall.csv', 'w') as f:
     for i in range(len(x)):
